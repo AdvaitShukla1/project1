bd91ffdfb3bd8f0ed3abf0e0aae2f6158e284011
Advait Shukla <notadvaitshukla@gmail.com>
2022-12-03 12:22:05 +0530

    "Single"

--- app1/views.py
+++ app1/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return render(request,'app1\\templates\\home.html')
+    return render(request,r'C:\Users\Advait Shukla\project1\app1\templates\app1\home.html')
 def about(request):
     return HttpResponse('<h1>Blog About</h1>')
