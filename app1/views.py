3ad72eba1c11072133928b6c6a3144d7498e1866
Advait Shukla <notadvaitshukla@gmail.com>
2022-12-03 12:55:14 +0530

    ""

--- app1/views.py
+++ app1/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+posts=[{'Author':'Advait Shukla',
+        'Title':'Blog Post 1',
+        'Content':'First post content',
+        'date_posted':'January 01,2022'},
+       {'Author':'Advait Shukla',
+        'Title':'Blog Post 2',
+        'Content':'Second post content',
+        'date_posted':'January 10,2022'}]
 # Create your views here.
 def home(request):
-    return render(request,r'C:\Users\Advait Shukla\project1\app1\templates\app1\home.html')
+    context={
+        'posts':posts
+    }
+    return render(request,r'app1\home.html',context)
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request,r'app1\about.html')
