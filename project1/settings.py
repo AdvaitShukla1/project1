1144c5c33e9e7a874a2fcf539938d93d05554005
Advait Shukla <notadvaitshukla@gmail.com>
2022-12-05 23:10:09 +0530

    6

--- project1/settings.py
+++ project1/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'app1.apps.App1Config',
     'django.contrib.admin',
     'django.contrib.auth',
